a87df0fcd1525baae83dd1d0cc2ef4b878856519
SHASHIKUMAR1121 <141137725+SHASHIKUMAR1121@users.noreply.github.com>
2023-09-21 02:21:35 +0000

    configure done

---
+++ 01_basics/var.js
@@ -0,0 +1,10 @@
+const accountNo =12345
+let accName ="shashi";
+var accId ="shashikumar599puc@gmail.com"; //do not use variables var//
+accPlace ="bengaluru";
+//console.table([accountNo,accName,accId,accPlace]);//
+
+accName ="shashi kumar";
+accPlace="bhoomi mele"
+accId ="shasshi@gmail.com"
+console.table([accountNo,accName,accId,accPlace]);
