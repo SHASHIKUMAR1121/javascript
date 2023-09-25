dfdecc6713d2da8720d9713ef77d8ed7018e74d8
SHASHIKUMAR1121 <141137725+SHASHIKUMAR1121@users.noreply.github.com>
2023-09-25 06:38:20 +0000

    done conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,23 @@
+/*let score ="123shasj";                   //converting number to string  //string to number //
+let newScore =String(score);            //   bollean values  true >>1 false >>0
+let numScore = Number(score);              //"num+string" == nan
+
+console.log(typeof(newScore));
+console.log(typeof(numScore));
+console.log(numScore);   
+console.log(typeof(score));          */
+ //
+
+ /*let a =0;
+ let b =Boolean(a);   // conversion   bollean values  true >>1 false >>0 empty >> false ; string >> true;
+
+ console.log(b);*/
+
+ let a = null;
+ let b = String(a); 
+ console.log(b);               // conversion of number, null , into strings
+ console.log(typeof(a));
+ console.log(typeof(b));
+
+
+
