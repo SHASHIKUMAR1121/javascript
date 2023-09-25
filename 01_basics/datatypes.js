b5bf1ee8844aee6e6261e8e3ab06359311377fe7
SHASHIKUMAR1121 <141137725+SHASHIKUMAR1121@users.noreply.github.com>
2023-09-25 05:59:52 +0000

    done dataypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+/* 
+2 types -
+1 -primitive  2 - non primitive 
+
+
+*/
+// let age = 25; //number 
+// let name ="shashi";// string
+// let goodBoy = true; //boolean
+// let attitue =BigInt("999")+BigInt("999"); //bigint
+// let behaviour =Symbol("smart and got boy ha"); // unique idnetification 
+// let anger =null; //null
+// let gf =undefined; //undefined
+// console.table([age,name,goodBoy,behaviour,anger,gf]); 
+
+const details ={
+    age : 25,                  //objects =key value pair 
+    name :"shashi" ,  
+    goodBoy : true , 
+    attitue :"awesome"
+   }
+   console.table(details["age"]);
+   
