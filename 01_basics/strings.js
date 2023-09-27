a98385006b593482b34cfcb547813f8613f3ecf2
SHASHIKUMAR1121 <141137725+SHASHIKUMAR1121@users.noreply.github.com>
2023-09-27 01:02:06 +0000

    done strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,19 @@
+let fname ="shashi";
+let lname =" kumar";
+
+let fullname =fname+lname;
+// console.log(fullname.length);
+// console.log(fullname.charAt(11));
+// console.log(fullname.indexOf("k"));
+// console.log(fullname.substring(0,5));
+// console.log(fullname.slice(7,11));
+// console.log(fullname.slice(7,12
+//     ));
+// console.log(fullname.trim());
+// console.log(fullname.includes("shashi"));
+// console.log(fullname.replace("shashi","kush").toUpperCase());
+// console.log(fullname.slice(-11,-5));
+console.log(fullname.slice(-12,-9
+
+    ));
+
